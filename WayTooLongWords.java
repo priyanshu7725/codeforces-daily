4a59147e4aa526b0b0e9b0358e96e412a8ffa5e0
Priyanshu Kumar <priyanshukumar3424@gmail.com>
2026-07-16 00:00:39 +0530

    Solved 71A - Way Too Long Words on 15th July

---
+++ WayTooLongWords.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class WayTooLongWords {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        if (sc.hasNextInt()) {
+            int n = sc.nextInt();
+            
+            for (int i = 0; i < n; i++) {
+                String word = sc.next();
+                int len = word.length();
+
+                if (len > 10) {
+                    System.out.println("" + word.charAt(0) + (len - 2) + word.charAt(len - 1));
+                } else {
+                    System.out.println(word);
+                }
+            }
+        }
+        sc.close();
+    }
+}
