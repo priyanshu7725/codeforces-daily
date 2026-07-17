6e7ef975b55c9a15c0c808b5d91b194ab575ae47
Priyanshu Kumar <priyanshukumar3424@gmail.com>
2026-07-17 23:29:12 +0530

    Solved 282A-Bill++ on 17 July

---
+++ BillPlus.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class BillPlus {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int x = 0;
+
+        while (n-- > 0) {
+            String s = sc.next();
+
+            if (s.contains("++")) {
+                x++;
+            } else {
+                x--;
+            }
+        }
+
+        System.out.println(x);
+        sc.close();
+    }
+}
