a6ab8951b95f54dd0cc3825a3e075b4798325ca5
priyanshu7725 <priyanshukumar3424@gmail.com>
2026-07-25 12:13:46 +0530

    Solved 617A - Elephant on 25 July

---
+++ Elephant.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Elephant {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        System.out.println((n + 4) / 5);
+
+        sc.close();
+
+    }
+}
