f7da1121a0594ea09a49b897d471f5bd7541cb82
priyanshu7725 <priyanshukumar3424@gmail.com>
2026-08-03 00:24:31 +0530

    Solved 467A - George and Accommodation on 3 August

---
+++ AGeorgeAndAccommodation.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class AGeorgeAndAccommodation {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        int count = 0;
+
+        while (n-- > 0) {
+
+            int p = sc.nextInt();
+            int q = sc.nextInt();
+
+            if (q-p >= 2) count++;
+
+        }
+
+        System.out.println(count);
+
+        sc.close();
+
+    }
+}
