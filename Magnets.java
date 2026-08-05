fcddfce9806ec1cb2ee521053773c5aabf8789b1
priyanshu7725 <priyanshukumar3424@gmail.com>
2026-08-05 22:19:54 +0530

    Solved 344A - Magnets on 5th August

--- Magnets.java
+++ Magnets.java
@@ -7,13 +7,17 @@ public class Magnets {
 
         int n = sc.nextInt()-1;
         
-        String prev = sc.next();
+        char last = sc.next().charAt(1);
         int res = 1;
 
-        while (--n > 0) {
-            String cur = sc.next();
-            if (!cur.equals(prev)) res++;
-            prev = cur;
+        while (n-- > 0) {
+
+            String s = sc.next();
+
+            if (last == s.charAt(0)) res++;
+
+            last = s.charAt(1);
+
         }
 
         System.out.println(res);
