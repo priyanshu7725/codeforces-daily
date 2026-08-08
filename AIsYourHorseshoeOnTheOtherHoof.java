ab8ae4fe0f5fe7a20c9f846c84c351768f4b63c9
priyanshu7725 <priyanshukumar3424@gmail.com>
2026-08-08 18:43:45 +0530

    Solved 228A - Is your horseshoe on the other hoof? on 8th August

---
+++ AIsYourHorseshoeOnTheOtherHoof.java
@@ -0,0 +1,17 @@
+import java.util.HashSet;
+import java.util.Scanner;
+
+public class AIsYourHorseshoeOnTheOtherHoof {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        HashSet<Integer> set = new HashSet<>();
+
+        for(int i = 0; i < 4; i++)
+            set.add(sc.nextInt());
+        
+        System.out.println(4 - set.size());
+        sc.close();
+
+    }
+}
