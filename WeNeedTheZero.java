0b9328fbb96ffd769bc7971f6865ee5de485f7ea
priyanshu7725 <priyanshukumar3424@gmail.com>
2026-08-28 14:51:19 +0530

    Solved 1805A - We Need the Zero on 28th August

---
+++ WeNeedTheZero.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class WeNeedTheZero {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+
+        while (t-- > 0) {
+            int n = sc.nextInt();
+            int xor = 0;
+
+            for (int i = 0; i < n; i++)
+                xor ^= sc.nextInt();
+
+            if (n % 2 == 1)
+                System.out.println(xor);
+            else if (xor == 0)
+                System.out.println(0);
+            else
+                System.out.println(-1);
+        }
+
+        sc.close();
+    }
+}
